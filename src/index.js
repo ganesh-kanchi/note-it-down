389386601b8ae65720b8dc66b9b8dd47cb1b798d
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2022-08-04 15:26:06 +0530

    [update] install and setup redux

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom";
+import { Provider } from "react-redux";
+import Store from "app/store";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={Store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
